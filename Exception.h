e7c10384bd22a4a338c0501e8c585c62e1708b41
Mauro7x <mparafati@fi.uba.ar>
2020-05-20 04:01:46 -0300

    abstracción de Exception genérica

---
+++ Exception.h
@@ -0,0 +1,43 @@
+#ifndef __EXCEPTION_H__
+#define __EXCEPTION_H__
+
+//-----------------------------------------------------------------------------
+#include <exception>
+#include <string>
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+
+/**
+ * Excepción general, permite definir un mensaje de error específico.
+*/
+class Exception : public std::exception {
+    private:
+        std::string msg_error;
+
+    public:
+        /** 
+         * Descripcion: constructor.
+         * Parametros: mensaje de error.
+        */
+        explicit Exception(const std::string msg_error) noexcept;
+
+
+        /** 
+         * Descripcion: devuelve el mensaje de error.
+         * Parametros: -
+         * Retorno: mensaje de error.
+         * 
+         * No hay que ocuparse del puntero recibido.
+        */
+        virtual const char* what() const noexcept;
+
+
+        /** 
+         * Descripcion: destructor.
+        */
+        virtual ~Exception() noexcept;
+};
+
+//-----------------------------------------------------------------------------
+#endif // __EXCEPTION_H__
