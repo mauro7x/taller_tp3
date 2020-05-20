cb297ceb656ec9166dce1acc74dbf8468e440cf3
Mauro7x <mparafati@fi.uba.ar>
2020-05-20 04:02:15 -0300

    estructura cliente y servidor main

---
+++ server_main.cpp
@@ -0,0 +1,34 @@
+/** TP3 - Taller de Programación - SERVIDOR
+ * 
+ * Alumno: Parafati, Mauro.
+ * Padrón: 102749.
+ * Año y cuatrimestre: 1C 2020.
+ * Repositorio: https://github.com/mauro7x/taller_tp3
+*/
+
+// ----------------------------------------------------------------------------
+#include <iostream>
+
+#include "defs.h"
+#include "Exception.h"
+// ----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+
+int main(int argc, char* argv[]) {
+
+    // Chequeamos argumentos recibidos
+    if (argc != 3) {
+        fprintf(stdout, "%s\n", USAGE_ERROR_MSG);
+        return USAGE_ERROR;
+    }
+
+
+
+
+
+    return 0;
+}
+
+// ----------------------------------------------------------------------------
+
