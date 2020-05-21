c51af1ccce8403ed708f271b7762207c3462408a
Mauro7x <mparafati@fi.uba.ar>
2020-05-20 21:10:00 -0300

    agrego mensaje de ayuda y tipos de comandos

--- defs.h
+++ defs.h
@@ -16,10 +16,19 @@
 // Mensajes de error
 #define USAGE_ERROR_MSG "Error: argumentos invalidos."
 
-
 // Mensajes del servidor
 #define WIN_MSG "Ganaste"
 #define LOSS_MSG "Perdiste"
+#define HELP_MSG "Comandos válidos:​\n​" \
+                  "\tAYUDA: despliega la lista de comandos válidos​\n"\
+                  "\t​RENDIRSE: pierde el juego automáticamente​\n"\
+                  "\tXXX: Número de 3 cifras a ser enviado al servidor"\
+                  "para adivinar el número secreto"
+
+// Tipos de comandos
+#define HELP 'h'
+#define SURRENDER 's'
+#define GUESS 'n'
 
 // ----------------------------------------------------------------------------
 
