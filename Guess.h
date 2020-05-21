788e67aefa34d1022d943cfd31486644b9a67f97
Mauro7x <mparafati@fi.uba.ar>
2020-05-21 04:10:16 -0300

    se implementa el comando () para guess con los metodos privados necesarios

--- Guess.h
+++ Guess.h
@@ -11,26 +11,75 @@ class Guess : public Command {
     private:
         unsigned short int value;
 
+        /**
+         * Description: chequea si el valor numérico es valido.
+         * 
+         * Parametros: -
+         * 
+         * Retorno: TRUE si el valor es valido, FALSE si no.
+        */
+        bool _isValid() const;
+
+        /**
+         * Description: cuenta la cantidad de digitos correctos.
+         * 
+         * Parametros: numero correcto para comparar.
+         * 
+         * Retorno: cantidad de digitos correctos, >= 0.
+        */
+        size_t _countCorrectDigits(unsigned short int secret_number) const;
+
+        /**
+         * Description: cuenta la cantidad de digitos regulares, es decir,
+         * aquellos que estén en el numero pero en otra posición.
+         * 
+         * Parametros: numero correcto para comparar.
+         * 
+         * Retorno: cantidad de digitos regulares, >= 0.
+        */
+        size_t _countRegularDigits(unsigned short int secret_number) const;
+
+        /**
+         * Description: formatea la respuesta segun el enunciado.
+         * 
+         * Parametros: respuesta a formatear pasada por referencia, cantidad
+         * de digitos correctos y cantidad de digitos regulares.
+         * 
+         * Retorno: -
+        */
+        void _formatReply(std::string& reply, size_t correct_digits,
+                          size_t regular_digits) const;
+
     public:
         /** 
          * Descripcion: constructor.
-         * Parametros: -
+         * 
+         * Parametros: valor numerico de 2 bytes del intento.
         */
         Guess(unsigned short int value);
 
         /**
          * Descripcion: retorna el valor numerico del intento.
+         * 
          * Parametros: -
+         * 
          * Retorno: valor numerico de 2 bytes.
         */
         unsigned short int number() const;
 
         /**
-         * Descripcion: ejecucion del comando Guess.
-         * Parametros: -
-         * Retorno: mensaje respuesta.
+         * Descripcion: se ejecuta el intento de adivinar el numero secreto
+         * de acuerdo a las reglas establecidas por el enunciado.
+         * 
+         * Parametros: numero secreto a adivinar, respuesta a llenar pasada
+         * por referencia, intentos restantes pasados por referencia.
+         * 
+         * Retorno: CONTINUE == continuar.
+         *          WIN == el cliente gano.
+         *          LOSS == el cliente perdió.
         */
-        std::string operator()(unsigned short secret_number) const override;
+        state operator()(unsigned short int secret_number, std::string& reply,
+                         unsigned int& remaining_attempts) const override;
 };
 
 // ----------------------------------------------------------------------------
--- common_Guess.cpp
+++ common_Guess.cpp
@@ -1,9 +1,75 @@
 #include "Guess.h"
 
-#include <iostream>
-
 // ----------------------------------------------------------------------------
 // Métodos privados
+
+bool Guess::_isValid() const {
+    if (value < MIN_ALLOWED_NUMBER || value > MAX_ALLOWED_NUMBER) {
+        return false;
+    }
+
+    std::string s_value = std::to_string(value);
+    char d1 = s_value[0], d2 = s_value[1], d3 = s_value[2];
+    return !(d1 == d2 || d1 == d3 || d2 == d3);
+}
+
+
+size_t Guess::_countCorrectDigits(unsigned short int secret_number) const {
+    size_t correct_digits = 0;
+
+    std::string s_value = std::to_string(value);
+    std::string s_correct = std::to_string(secret_number);
+
+    // digitos del intento:
+    char d1 = s_value[0], d2 = s_value[1], d3 = s_value[2];    
+
+    // digitos correctos:
+    char c1 = s_correct[0], c2 = s_correct[1], c3 = s_correct[2];
+
+    correct_digits += (int) (d1 == c1);
+    correct_digits += (int) (d2 == c2);
+    correct_digits += (int) (d3 == c3);
+
+    return correct_digits;
+}
+
+
+size_t Guess::_countRegularDigits(unsigned short int secret_number) const {
+    size_t regular_digits = 0;
+
+    std::string s_value = std::to_string(value);
+    std::string s_correct = std::to_string(secret_number);
+
+    // digitos del intento:
+    char d1 = s_value[0], d2 = s_value[1], d3 = s_value[2];    
+
+    // digitos correctos:
+    char c1 = s_correct[0], c2 = s_correct[1], c3 = s_correct[2];
+
+    regular_digits += (int) (d1 == c2 || d1 == c3);
+    regular_digits += (int) (d2 == c1 || d2 == c3);
+    regular_digits += (int) (d3 == c1 || d3 == c2);
+
+    return regular_digits;
+}
+
+
+void Guess::_formatReply(std::string& reply, size_t correct_digits,
+                          size_t regular_digits) const {
+    reply.clear();
+    std::string s_correct = std::to_string(correct_digits);
+    std::string s_regular = std::to_string(regular_digits);
+
+    if (correct_digits > 0 && regular_digits > 0) {
+        reply = s_correct + " bien, " + s_regular + " regular";
+    } else if (correct_digits > 0) {
+        reply = s_correct + " bien";
+    } else if (regular_digits > 0) {
+        reply = s_regular + " regular";
+    } else {
+        reply = "3 mal";
+    }
+}
 
 
 // ----------------------------------------------------------------------------
@@ -17,11 +83,30 @@ unsigned short int Guess::number() const {
 }
 
 
-std::string Guess::operator()(unsigned short secret_number) const {
-    // GUESS ACTION
-    
-    return "Value: " + std::to_string(value) + ".";
-}
+state Guess::operator()(unsigned short int secret_number, std::string& reply,
+                        unsigned int& remaining_attempts) const {
+    if (!_isValid()) {
+        reply.clear();
+        reply = INVALID_NUMBER_ERROR_MSG;
+        return CONTINUE;
+    }
 
+    if (value == secret_number) {
+        reply.clear();
+        reply = WIN_MSG;
+        return WIN;
+    }
+
+    if (--remaining_attempts == 0) {
+        reply.clear();
+        reply = LOSS_MSG;
+        return LOSS;
+    }
+
+    size_t correct_digits = _countCorrectDigits(secret_number);
+    size_t regular_digits = _countRegularDigits(secret_number);
+    _formatReply(reply, correct_digits, regular_digits);
+    return CONTINUE;
+}
 
 // ----------------------------------------------------------------------------
