187294878cf638b4706b730475a8a688e29a36cc
Mauro7x <mparafati@fi.uba.ar>
2020-05-21 04:08:35 -0300

    saco defines propios de la lógica de ciertos objetos

--- defs.h
+++ defs.h
@@ -17,29 +17,25 @@
 #define USAGE_ERROR_MSG "Error: argumentos invalidos."
 #define UNKNOWN_COMMAND_ERROR_MSG "Error: comando inválido. "\
                                   "Escriba AYUDA para obtener ayuda"
+#define INVALID_NUMBER_ERROR_MSG "Número inválido. "\
+                                 "Debe ser de 3 cifras no repetidas"
 
-// Mensajes del servidor
+// Mensajes del servidor fijos
 #define WIN_MSG "Ganaste"
 #define LOSS_MSG "Perdiste"
 #define HELP_MSG "Comandos válidos:​\n​" \
                   "\tAYUDA: despliega la lista de comandos válidos​\n"\
                   "\t​RENDIRSE: pierde el juego automáticamente​\n"\
-                  "\tXXX: Número de 3 cifras a ser enviado al servidor"\
+                  "\tXXX: Número de 3 cifras a ser enviado al servidor "\
                   "para adivinar el número secreto"
 
-// Tipos de comandos
-#define HELP 'h'
-#define SURRENDER 's'
-#define GUESS 'n'
-
-// Input de comandos correcto
-#define HELP_INPUT "AYUDA"
-#define SURRENDER_INPUT "RENDIRSE"
+// Rangos de numeros permitidos
+#define MIN_ALLOWED_NUMBER 100
+#define MAX_ALLOWED_NUMBER 999
 
 // Rangos de representacion de unsigned short int
 #define MIN_POSSIBLE_NUMBER 0
 #define MAX_POSSIBLE_NUMBER 65535
-
 // ----------------------------------------------------------------------------
 
 // ----------------------------------------------------------------------------
