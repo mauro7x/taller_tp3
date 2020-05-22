69b586073eeac4f10b4cb5f0ffcf9b5344791cce
Mauro7x <mparafati@fi.uba.ar>
2020-05-22 03:30:40 -0300

    implemento el parser de numeros

---
+++ Numbers.h
@@ -0,0 +1,91 @@
+#ifndef __NUMBERS_H__
+#define __NUMBERS_H__
+
+// ----------------------------------------------------------------------------
+#include <string>
+#include <iterator>
+#include <list>
+#include <cstdint>
+#include <fstream>
+
+#include "InputErrorException.h"
+#include "defs.h"
+// ----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+
+class Numbers {
+    private:
+        std::list<uint16_t> numbers;
+        std::list<uint16_t>::iterator it;
+
+        /**
+         * Descripcion: chequea que sea un numero valido.
+         * 
+         * Parametros: numero a chequear.
+         * 
+         * Retorno: -
+         * 
+         * Si el numero no está en el rango permitido o tiene
+         * cifras repetidas, lanza excepcion InputErrorException.
+         * 
+         * >THROW EXPLICITO DE EXCEPTION.
+        */
+        void _checkIfValid(const uint64_t& input_number) const;
+
+        /**
+         * Descripcion: parsea los numeros en el archivo, chequeando
+         * que su formato sea correcto, y los agrega a la lista.
+         * 
+         * Parametros: archivo abierto.
+         * 
+         * Retorno: -
+         * 
+         * Si algun numero no cumple con el formato pedido, se lanza
+         * una excepción.
+         * 
+         * >THROW EXPLICITO DE EXCEPTION.
+        */
+        void _parseNumbers(std::ifstream& file);
+
+    public:
+        /** 
+         * Descripcion: constructor. Abre el archivo y arma la lista de
+         * numeros.
+         * 
+         * Parametros: ruta al archivo pasada por referencia.
+         * 
+         * >THROW EXPLICITO DE EXCEPTION.
+        */
+        Numbers(const std::string& filepath);
+
+        /** 
+         * Deshabilitamos el constructor por copia y su operador.
+        */
+        Numbers(const Numbers&) = delete;
+        Numbers& operator=(const Numbers&) = delete;
+
+        /** 
+         * Deshabilitamos el constructor por movimiento y su operador.
+        */
+        Numbers(Numbers&& other) = delete;
+        Numbers& operator=(Numbers&& other) = delete;
+
+        /**
+         * Descripcion: devuelve el proximo numero a utilizar, obtenido
+         * de forma round robin.
+         * 
+         * Parametros: -
+         * 
+         * Retorno: numero secreto a utilizar, uint16_t
+        */
+        uint16_t operator()();
+
+        /** 
+         * Descripcion: destructor.
+        */
+        ~Numbers();
+};
+
+// ----------------------------------------------------------------------------
+#endif // __NUMBERS_H__
---
+++ server_Numbers.cpp
@@ -0,0 +1,68 @@
+#include "Numbers.h"
+
+
+#include <iostream>
+
+// ----------------------------------------------------------------------------
+// Métodos privados
+
+void Numbers::_checkIfValid(const uint64_t& input_number) const {
+    if (input_number < MIN_ALLOWED_NUMBER || input_number > MAX_ALLOWED_NUMBER) {
+        throw InputErrorException(OUT_OF_RANGE_INPUT_MSG);
+    }
+
+    std::string s_input_number = std::to_string(input_number);
+    char d1 = s_input_number[0], d2 = s_input_number[1], d3 = s_input_number[2];
+
+    if (d1 == d2 || d1 == d3 || d2 == d3) {
+        throw InputErrorException(INVALID_FORMAT_INPUT_MSG);
+    }
+}
+
+
+void Numbers::_parseNumbers(std::ifstream& file) {
+    uint64_t input_number;
+    uint16_t parsed_number;
+    while (file >> input_number) {
+        _checkIfValid(input_number);
+        parsed_number = (uint16_t) input_number;
+        numbers.push_back(parsed_number);
+    }
+}
+
+
+// ----------------------------------------------------------------------------
+// API pública
+
+Numbers::Numbers(const std::string& filepath) {
+    std::ifstream file;
+
+    file.open(filepath);
+    if (!file.is_open()) {
+        throw InputErrorException("El archivo no se pudo abrir.");
+    }
+
+    _parseNumbers(file);
+    it = numbers.begin();
+
+    file.close();
+    if (file.is_open()) {
+        throw InputErrorException("El archivo no se pudo cerrar.");
+    }
+
+}
+
+
+uint16_t Numbers::operator()() {
+    uint16_t n = *(it++);
+    if (it == numbers.end()) {
+        it = numbers.begin();
+    }
+    return n;
+}
+
+
+Numbers::~Numbers() {}
+
+
+// ----------------------------------------------------------------------------
