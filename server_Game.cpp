a91d5f59bbd0582e0d7c0a6afc543543288217f4
Mauro7x <mparafati@fi.uba.ar>
2020-05-22 18:41:44 -0300

    formateo con Google Style

--- server_Game.cpp
+++ server_Game.cpp
@@ -3,16 +3,16 @@
 // ----------------------------------------------------------------------------
 // Métodos privados
 
-
 // ----------------------------------------------------------------------------
 // API pública
 
 ServerGame::ServerGame(int fd, ProtectedResults& results,
-                       uint16_t secret_number) :
-                       protocol(fd), results(results),
-                       secret_number(secret_number),
-                       remaining_attempts(ATTEMPTS), is_running(true) {}
-
+                       uint16_t secret_number)
+    : protocol(fd),
+      results(results),
+      secret_number(secret_number),
+      remaining_attempts(ATTEMPTS),
+      is_running(true) {}
 
 void ServerGame::run() {
     try {
@@ -56,16 +56,13 @@ void ServerGame::run() {
     is_running = false;
 }
 
-
 bool ServerGame::isOver() {
     return !is_running;
 }
 
-
 void ServerGame::stop() {
     protocol.stop();
 }
-
 
 ServerGame::~ServerGame() {}
 
