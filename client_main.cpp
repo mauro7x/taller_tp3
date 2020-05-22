888a4ec315e1d5853871e92849abb35b29a6f1bb
Mauro7x <mparafati@fi.uba.ar>
2020-05-22 06:26:21 -0300

    agrego mensaje de error desconocido

--- client_main.cpp
+++ client_main.cpp
@@ -33,6 +33,7 @@ int main(int argc, char* argv[]) {
         std::cerr << e.what() << "\n";
         return ERROR;
     } catch (...) {
+        std::cerr << "Error desconocido." << '\n';
         return ERROR;
     }
 
